38103ee45934bdf9c6cfae61185b61063311a34c
Tieshow Daniels <tieshowdaniels@Tieshows-MacBook-Pro.local>
2017-04-20 20:34:43 -0700

    Adds FSAPI+Venues to convert JSON to 'Venue' objects

---
+++ FSAPI+Venues.swift
@@ -0,0 +1,24 @@
+//
+//  FSAPI+Venues.swift
+//  ios-interview
+//
+//  Created by Tieshow Daniels on 4/20/17.
+//  Copyright © 2017 Foursquare. All rights reserved.
+//
+
+import Foundation
+
+//Spoofed API base to share JSON deserializer throughout the application
+struct FSAPI {}
+
+extension FSAPI {
+    
+    //Creates venue object from locally store JSON data file
+    //
+    // - If JSON is readable into a venue object, returns an optional FSVenue object
+    struct Venues {
+        
+//        static func getVenueFromJSON() -> Venue? {}
+    }
+}
+
--- FSJSON.swift
+++ FSJSON.swift
@@ -10,6 +10,10 @@ import Foundation
 
 class FSJSON {
     
+//    MARK: - JSON deserialization
+//    Method to check whether JSON is capable of being serialized into a dictionary
+//    
+//    Input will be a set of data in the form of JSON, the output will be a JSON deserialized into a dictionary
     static func deserializeJSON(data: Data?) -> [String : Any]? {
         guard let data = data, data.count != 0 else {
             return nil
