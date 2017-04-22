b6a590bff840b48f3d5efc008e92e0d8675afb2b
Tieshow Daniels <tieshowdaniels@Tieshows-MacBook-Pro.local>
2017-04-22 16:26:45 -0700

    Optimized 0n2 solution

--- FSVenue.swift
+++ FSVenue.swift
@@ -63,24 +63,41 @@ fileprivate extension FSVenue {
          - Gaps are inserted based on lack of entry time or immediately after someone exits until the next person or closing
          */
         
-        //Sorted by arrivalTime
-        var venueVisitors = visitors?.sorted(by: {$0.arriveTime < $1.arriveTime})
+        // MARK - Instatiate sorted arrays used within the algorithm
+        guard var sortedFirst = visitors?.sorted(by: {$0.arriveTime < $1.arriveTime}),
+            var sortedLast = visitors?.sorted(by: {$0.leaveTime < $1.leaveTime}),
+            var scheduleWithGaps = visitors?.sorted(by: {$0.arriveTime < $1.arriveTime}) else { return nil
         
-        // NOTE: - On^2 solutionm
-        for index in 0..<(venueVisitors?.count)! - 1 {
-            guard let thisVisitor = venueVisitors?[index], let nextVisitor = venueVisitors?[index + 1] else {
-                return nil
-            }
-
-            //Skip if theres overlap between current and next visitor's start and end times
-            if (thisVisitor.leaveTime.isLess(than: nextVisitor.leaveTime)) && (thisVisitor.leaveTime.isLess(than: nextVisitor.arriveTime)) {
-                let visitorLeave = thisVisitor.leaveTime
-                let nextVisitorArrival = nextVisitor.arriveTime
-                
-                let fillerVisitor = FSVenueVisitorGap(arriveTime: visitorLeave, leaveTime: nextVisitorArrival)!
-                venueVisitors?.append(fillerVisitor)
+        }
+        
+        // - Array of 'FSVenueVisitor' sorted start/end times
+        var sortedByStartAndEnd = [FSVenueVisitor]()
+        
+        // - Gap before first guest
+        let firstGap = FSVenueVisitorGap(arriveTime: open, leaveTime: (scheduleWithGaps.first?.arriveTime)!)
+        scheduleWithGaps.append(firstGap!)
+        
+        // - Create array with sorted start/end times
+        for index in 0..<(sortedLast.count) {
+            
+            let gapVisitor = FSVenueVisitorGap(arriveTime: (sortedFirst[index].arriveTime), leaveTime: (sortedLast[index].leaveTime))
+            sortedByStartAndEnd.append(gapVisitor!)
+        }
+        
+        let lastGap = FSVenueVisitorGap(arriveTime: (sortedByStartAndEnd.last?.leaveTime)!, leaveTime: close)
+        scheduleWithGaps.append(lastGap!)
+        
+        for i in 1..<(sortedByStartAndEnd.count) {
+            
+            let beginningOfGap = sortedByStartAndEnd[i - 1].leaveTime
+            let endOfGap = sortedByStartAndEnd[i].arriveTime
+            
+            if (beginningOfGap < endOfGap) {
+                let overlapping = FSVenueVisitorGap(arriveTime: beginningOfGap, leaveTime: endOfGap)
+                scheduleWithGaps.append(overlapping!)
             }
         }
-        return venueVisitors
+        
+        return scheduleWithGaps.sorted(by: {$0.arriveTime < $1.arriveTime})
     }
 }
